211522feb02c2430bda0b6ba2d3d116e31761790
axel10 <zgmf300@outlook.com>
2026-05-22 10:33:12 +0800

    Initial Commit

---
+++ testSFBAE/testSFBAEApp.swift
@@ -0,0 +1,17 @@
+//
+//  testSFBAEApp.swift
+//  testSFBAE
+//
+//  Created by axel10 on 2026/5/22.
+//
+
+import SwiftUI
+
+@main
+struct testSFBAEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
